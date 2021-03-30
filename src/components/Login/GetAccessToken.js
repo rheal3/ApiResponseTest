c51417bff396bedf8b77166700f3509396b56907
Rebekah Heal <heal.rebekah@gmail.com>
2021-03-30 10:37:18 +1000

    added apiToken & BASE_URL  to local storage

---
+++ src/components/Login/GetAccessToken.js
@@ -0,0 +1,21 @@
+let apiToken;
+
+function getAccessToken(username, password) {
+    let url = localStorage.getItem('BASE_URL') + "/auth";
+    fetch(url, {
+        body: `grant_type=password&username=${username}&password=${password}`,
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        method: "POST"
+    }).then(response => {
+        return response.json()
+    }).then(data => {
+        apiToken = data.access_token;
+        console.log(apiToken)
+        localStorage.setItem('apiToken', apiToken)
+        alert('Token Acquired.')
+    }).catch(error => console.log(error))
+}
+
+export default getAccessToken
