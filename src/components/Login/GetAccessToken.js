10cddfc5b3e59f6d85e11bc02d62f42731fbf382
Rebekah Heal <heal.rebekah@gmail.com>
2021-03-30 11:40:57 +1000

    added timer to getAccessToken()

--- src/components/Login/GetAccessToken.js
+++ src/components/Login/GetAccessToken.js
@@ -2,6 +2,9 @@ let apiToken;
 
 function getAccessToken(username, password) {
     let url = sessionStorage.getItem('BASE_URL') + "/auth";
+
+    let startTime = window.performance.now()
+
     fetch(url, {
         body: `grant_type=password&username=${username}&password=${password}`,
         headers: {
@@ -9,10 +12,14 @@ function getAccessToken(username, password) {
         },
         method: "POST"
     }).then(response => {
+
+        let endTime = window.performance.now()
+        let time = endTime - startTime
+        console.log("Get API Token Response Execution Time: " + time + " milliseconds")
+
         return response.json()
     }).then(data => {
         apiToken = data.access_token;
-        console.log(apiToken)
         sessionStorage.setItem('apiToken', apiToken)
         alert('Token Acquired.')
     }).catch(error => console.log(error))
