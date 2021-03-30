733450c407ba9b7f368ff7ed223253ec492321fa
Rebekah Heal <heal.rebekah@gmail.com>
2021-03-30 15:39:47 +1000

    updated getUsers()

--- src/components/ApiCalls/GetUsers.js
+++ src/components/ApiCalls/GetUsers.js
@@ -5,34 +5,33 @@ function getUsersInGroup() {
     let apiToken = sessionStorage.getItem('apiToken')
     if (!sessionStorage['groupObj']) {
         getGroups()
-    } else {
-        let groupObj = JSON.parse(sessionStorage.getItem('groupObj'))
+    }
+    let groupObj = JSON.parse(sessionStorage.getItem('groupObj'))
 
-        for (let i = 0; i < groupObj.groups.length; i++) {
-            let groupId = groupObj.groups[i].id
-            let groupName = groupObj.groups[i].name
-    
-            let url = sessionStorage.getItem('BASE_URL')  + `/groups/${groupId}/users`;
-    
-            const startTime = window.performance.now()
-            let time;
-    
-            fetch(url, {
-                headers: {
-                    "Content-Type": "application/json",
-                    "Authorization": "Bearer " + apiToken
-                }
-            }).then(response => {
-    
-                let endTime = window.performance.now()
-                time = endTime - startTime
-    
-                return response.json()
-            }).then(data => {
-                alert(`API call to get users from ${groupName} returned ${data.users.length} users in ${time} milliseconds`)
-                return data
-            })
-        } 
+    for (let i = 0; i < groupObj.groups.length; i++) {
+        let groupId = groupObj.groups[i].id
+        let groupName = groupObj.groups[i].name
+
+        let url = sessionStorage.getItem('BASE_URL')  + `/groups/${groupId}/users`;
+
+        const startTime = window.performance.now()
+        let time;
+
+        fetch(url, {
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer " + apiToken
+            }
+        }).then(response => {
+
+            let endTime = window.performance.now()
+            time = endTime - startTime
+
+            return response.json()
+        }).then(data => {
+            alert(`API call to get users from ${groupName} returned ${data.users.length} users in ${time} milliseconds`)
+            return data
+        })
     } 
 }
 
