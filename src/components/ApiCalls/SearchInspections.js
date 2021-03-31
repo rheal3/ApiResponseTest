e23689de6d1c1007a1c2c9d0ef919aaf1055d90f
macca-lachlan <30332634+macca-lachlan@users.noreply.github.com>
2021-03-31 13:19:41 +1000

    have test template time and test inspection time print results to screen

--- src/components/ApiCalls/SearchInspections.js
+++ src/components/ApiCalls/SearchInspections.js
@@ -1,58 +1,66 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { storeData, dateTime } from '../dataStorage'
 
-function searchInspections () {
-    if (!sessionStorage['apiToken']) {
-        return alert("LOGIN FOR COOL FEATURES!")
-    } else {
-        let apiToken = sessionStorage.getItem('apiToken')
-        let url = sessionStorage.getItem('BASE_URL') + '/audits/search?field=audit_id&field=modified_at'
-
-        const startTime = window.performance.now()
-        let time;
-    
-        fetch(url, {
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": "Bearer " + apiToken
-            }
-        }).then(response => {    
-            let endTime = window.performance.now()
-            time = endTime - startTime
-
-            if (response.ok) {
-                return response.json()
-            } else {
-                return false
-            }
-        }).then(data => {
-            if (data !== false) {
-                storeData({
-                    process: 'searchInspections',
-                    responseOk: true,
-                    time: `${Math.round((time + Number.EPSILON) * 100) / 100} ms`,
-                    numItemsRetrieved: data.count,
-                    dateTime: dateTime(),
-                })
-            } else {
-                storeData({
-                    process: 'searchInspections',
-                    responseOk: false,
-                    time: `${Math.round((time + Number.EPSILON) * 100) / 100} ms`,
-                    numItemsRetrieved: 0,
-                    dateTime: dateTime(),
-                })
-            }
-            return data
-        }).catch(error => console.log(error))
-    }
-}
-
-
 const SearchInspections = () => {
+    const [getInspectionTime, setInspectionTime] = useState('')
+
+    const getInspections = () => {
+        if (!sessionStorage['apiToken']) {
+            return alert("LOGIN FOR COOL FEATURES!")
+        } else {
+            let apiToken = sessionStorage.getItem('apiToken')
+            let url = sessionStorage.getItem('BASE_URL') + '/audits/search?field=audit_id&field=modified_at'
+    
+            const startTime = window.performance.now()
+            let time;
+        
+            fetch(url, {
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": "Bearer " + apiToken
+                }
+            }).then(response => {    
+                let endTime = window.performance.now()
+                time = endTime - startTime
+    
+                if (response.ok) {
+                    setInspectionTime(time + " milliseconds")
+                    return response.json()
+                } else {
+                    return false
+                }
+            }).then(data => {
+                if (data !== false) {
+                    storeData({
+                        process: 'searchInspections',
+                        responseOk: true,
+                        time: `${Math.round((time + Number.EPSILON) * 100) / 100} ms`,
+                        numItemsRetrieved: data.count,
+                        dateTime: dateTime(),
+                    })
+                } else {
+                    storeData({
+                        process: 'searchInspections',
+                        responseOk: false,
+                        time: `${Math.round((time + Number.EPSILON) * 100) / 100} ms`,
+                        numItemsRetrieved: 0,
+                        dateTime: dateTime(),
+                    })
+                }
+                return data
+            }).catch(error => console.log(error))
+        }
+    }
+
+
     return (
         <div>
-            <button onClick={searchInspections}>Search Inspections</button>
+            <div>
+            <h3>Search Inspections response time test</h3>
+            <button onClick={getInspections}>Search Inspections</button>
+            </div>
+            <p>Search Inspections Time: {getInspectionTime}</p>
+            
         </div>
     )
 }
--- src/components/ApiCalls/SearchTemplates.js
+++ src/components/ApiCalls/SearchTemplates.js
@@ -1,58 +1,65 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { storeData, dateTime } from '../dataStorage'
 
-function searchTemplates () {
-    if (!sessionStorage['apiToken']) {
-        return alert("LOGIN FOR COOL FEATURES!")
-    } else {
-        let apiToken = sessionStorage.getItem('apiToken')
-        let url = sessionStorage.getItem('BASE_URL') + '/templates/search?field=template_id&field=modified_at'
-
-        const startTime = window.performance.now()
-        let time;
-    
-        fetch(url, {
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": "Bearer " + apiToken
-            }
-        }).then(response => {    
-            let endTime = window.performance.now()
-            time = endTime - startTime
-
-            if (response.ok) {
-                return response.json()
-            } else {
-                return false
-            }
-        }).then(data => {
-            if (data !== false) {
-                storeData({
-                    process: 'searchTemplates',
-                    responseOk: true,
-                    time: `${Math.round((time + Number.EPSILON) * 100) / 100} ms`,
-                    numItemsRetrieved: data.count,
-                    dateTime: dateTime(),
-                })
-            } else {
-                storeData({
-                    process: 'searchTemplates',
-                    responseOk: false,
-                    time: `${Math.round((time + Number.EPSILON) * 100) / 100} ms`,
-                    numItemsRetrieved: 0,
-                    dateTime: dateTime(),
-                })
-            }
-            return data
-        }).catch(error => console.log(error))
-    }
-}
-
-
 const SearchTemplates = () => {
+    const [getTemplateTime, setTemplateTime] = useState('')
+
+    const getTemplates = () => {
+        if (!sessionStorage['apiToken']) {
+            return alert("LOGIN FOR COOL FEATURES!")
+        } else {
+            let apiToken = sessionStorage.getItem('apiToken')
+            let url = sessionStorage.getItem('BASE_URL') + '/templates/search?field=template_id&field=modified_at'
+    
+            const startTime = window.performance.now()
+            let time;
+        
+            fetch(url, {
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": "Bearer " + apiToken
+                }
+            }).then(response => {    
+                let endTime = window.performance.now()
+                time = endTime - startTime
+    
+                if (response.ok) {
+                    setTemplateTime(time + " milliseconds")
+                    return response.json()
+                } else {
+                    return false
+                }
+            }).then(data => {
+                if (data !== false) {
+                    storeData({
+                        process: 'searchTemplates',
+                        responseOk: true,
+                        time: `${Math.round((time + Number.EPSILON) * 100) / 100} ms`,
+                        numItemsRetrieved: data.count,
+                        dateTime: dateTime(),
+                    })
+                } else {
+                    storeData({
+                        process: 'searchTemplates',
+                        responseOk: false,
+                        time: `${Math.round((time + Number.EPSILON) * 100) / 100} ms`,
+                        numItemsRetrieved: 0,
+                        dateTime: dateTime(),
+                    })
+                }
+                return data
+            }).catch(error => console.log(error))
+        }
+    }
+
+
     return (
         <div>
-            <button onClick={searchTemplates}>Search Templates</button>
+            <div>
+            <h3>Search Templates response time test</h3>
+            <button onClick={getTemplates}>Search Templates</button>
+            </div>
+            <p>Search Templates Time: {getTemplateTime}</p>
         </div>
     )
 }
