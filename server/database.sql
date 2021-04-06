78f9700a0009bab73888f674dd704fd990e452cd
Rebekah Heal <heal.rebekah@gmail.com>
2021-04-07 08:59:30 +1000

    updated database

--- server/database.sql
+++ server/database.sql
@@ -1,8 +1,36 @@
 CREATE DATABASE sc_database;
 
--- \c sc_database
+\c sc_database
 
-CREATE TABLE ${tableName} (
+CREATE TABLE access_token (
+    id SERIAL PRIMARY KEY,
+    response_ok BOOLEAN,
+    time VARCHAR(30),
+    num_items_retrieved INTEGER,
+    date_time VARCHAR(30)
+);
+CREATE TABLE groups (
+    id SERIAL PRIMARY KEY,
+    response_ok BOOLEAN,
+    time VARCHAR(30),
+    num_items_retrieved INTEGER,
+    date_time VARCHAR(30)
+);
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    response_ok BOOLEAN,
+    time VARCHAR(30),
+    num_items_retrieved INTEGER,
+    date_time VARCHAR(30)
+);
+CREATE TABLE templates (
+    id SERIAL PRIMARY KEY,
+    response_ok BOOLEAN,
+    time VARCHAR(30),
+    num_items_retrieved INTEGER,
+    date_time VARCHAR(30)
+);
+CREATE TABLE inspections (
     id SERIAL PRIMARY KEY,
     response_ok BOOLEAN,
     time VARCHAR(30),
@@ -10,9 +38,4 @@ CREATE TABLE ${tableName} (
     date_time VARCHAR(30)
 );
 
-/* tables: (\dt)
-access_token
-groups
-users
-templates
-inspections */
+\dt
