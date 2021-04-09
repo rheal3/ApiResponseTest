71b9137ca561a59a0827349aa5c0f9a9922bbcee
Rebekah Heal <heal.rebekah@gmail.com>
2021-04-09 13:47:45 +1000

    correct db details

--- server/db.js
+++ server/db.js
@@ -2,8 +2,8 @@ const Pool = require("pg").Pool;
 
 // Connection details for the database
 const pool = new Pool({
-    user: "postgres",
-    password: "M@ccat3ch",
+    user: "admin",
+    password: "purplelobstermountain",
     host: "localhost",
     port: 5432,
     database: "sc_database"
