931e0e24c4401e033c916f7c925972e0f12d6b15
macca-lachlan <30332634+macca-lachlan@users.noreply.github.com>
2021-04-12 14:57:57 +1000

    add layout to results page

--- react-postgres/src/components/Results/Results.js
+++ react-postgres/src/components/Results/Results.js
@@ -1,10 +1,128 @@
 import React from 'react'
 
 const Results = () => {
-
+// will need to get the required data for each of the charts and setup in format that chart can read from
     return (
         <div>
-            The Results page to be
+            <div style={{backgroundColor: 'lightsalmon'}}>
+                24hr chart spot
+
+                <div>
+                    main chart
+                </div>
+
+                <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly', flexWrap: 'nowrap' }}>
+                    <div style={{ textAlign: 'center' }}>
+                        Login chart                      
+                    </div>
+
+                    <div style={{ textAlign: 'center' }}>
+                       Group chart                      
+                    </div>
+
+                    <div style={{ textAlign: 'center' }}>
+                       User chart                      
+                    </div>
+
+                    <div style={{ textAlign: 'center' }}>
+                       Template chart                      
+                    </div>
+
+                    <div style={{ textAlign: 'center' }}>
+                        Inspection chart                      
+                    </div>
+                </div>
+            </div>
+
+            <div  style={{backgroundColor: 'lightblue'}}>
+                1 week chart spot
+
+                <div>
+                    main chart
+                </div>
+
+                <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly', flexWrap: 'nowrap' }}>
+                    <div style={{ textAlign: 'center' }}>
+                        Login chart                      
+                    </div>
+
+                    <div style={{ textAlign: 'center' }}>
+                       Group chart                      
+                    </div>
+
+                    <div style={{ textAlign: 'center' }}>
+                       User chart                      
+                    </div>
+
+                    <div style={{ textAlign: 'center' }}>
+                       Template chart                      
+                    </div>
+
+                    <div style={{ textAlign: 'center' }}>
+                        Inspection chart                      
+                    </div>
+                </div>
+            </div>
+
+            <div style={{backgroundColor: 'lightgoldenrodyellow'}}>
+                1 fortnight chart spot
+
+                <div>
+                    main chart
+                </div>
+
+                <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly', flexWrap: 'nowrap' }}>
+                    <div style={{ textAlign: 'center' }}>
+                        Login chart                      
+                    </div>
+
+                    <div style={{ textAlign: 'center' }}>
+                       Group chart                      
+                    </div>
+
+                    <div style={{ textAlign: 'center' }}>
+                       User chart                      
+                    </div>
+
+                    <div style={{ textAlign: 'center' }}>
+                       Template chart                      
+                    </div>
+
+                    <div style={{ textAlign: 'center' }}>
+                        Inspection chart                      
+                    </div>
+                </div>
+            </div>
+
+            <div style={{backgroundColor: 'lightgreen'}}>
+                1 month chart spot
+
+                <div>
+                    main chart
+                </div>
+
+                <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-evenly', flexWrap: 'nowrap' }}>
+                    <div style={{ textAlign: 'center' }}>
+                        Login chart                      
+                    </div>
+
+                    <div style={{ textAlign: 'center' }}>
+                       Group chart                      
+                    </div>
+
+                    <div style={{ textAlign: 'center' }}>
+                       User chart                      
+                    </div>
+
+                    <div style={{ textAlign: 'center' }}>
+                       Template chart                      
+                    </div>
+
+                    <div style={{ textAlign: 'center' }}>
+                        Inspection chart                      
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
